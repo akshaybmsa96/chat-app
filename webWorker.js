740bd0fdb3bfb525a94f3344605782715e484a31
Akshay <akshaybmsa96@gmail.com>
2022-01-26 03:27:39 +0530

    web worker added

---
+++ webWorker.js
@@ -0,0 +1,48 @@
+let socket = null;
+
+const estabilishConnection = () => {
+  socket = new WebSocket("wss://javascript.info/article/websocket/chat/ws");
+
+  socket.addEventListener("open", () => {
+    console.log("Connection estabilished with server");
+    postMessage(JSON.stringify({ type: "OPEN" }));
+  });
+
+  socket.addEventListener("close", () => {
+    console.log("Connection closed with server");
+    postMessage(JSON.stringify({ type: "CLOSE" }));
+  });
+
+  socket.addEventListener("error", function (event) {
+    console.log("Got error: ", event);
+    postMessage(JSON.stringify({ type: "ERROR" }));
+  });
+
+  socket.onmessage = function (event) {
+    let message = event.data;
+    postMessage(JSON.stringify({ type: "RECEIVED", msg: message }));
+  };
+};
+
+onmessage = function (e) {
+  console.log("Worker: Message received from main script");
+  const data = JSON.parse(e.data);
+
+  switch (data.type) {
+    case "start":
+      estabilishConnection();
+      break;
+    case "send":
+      if (socket !== null) {
+        socket.send(data.msg);
+      }
+      break;
+    case "stop":
+      if (socket !== null) {
+        socket.close();
+      }
+      break;
+    default:
+      break;
+  }
+};
